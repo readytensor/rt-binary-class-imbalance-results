69cdca4f23250544bb43d6e34af09b359b7e6f6d
rt-desai <abhyuday.desai@readytensor.com>
2024-08-27 10:15:18 -0700

    add table svg generator

--- src/config/chart_cfg.py
+++ src/config/chart_cfg.py
@@ -14,3 +14,19 @@ colors = {
     "SMOTE": "#FF9B61",
     "Tie": "#B3BEC4",
 }
+
+
+# Default table styling
+header_font_size = 20
+table_font_size = 18
+table_first_col_font_size = 18
+table_background_color = "#f9f9f9"
+table_background_color2 = "#e6e6e6"
+table_first_row_background_color = "#F0F0F0"
+table_first_col_background_color = "#F0F0F0"
+table_outer_border_thickness = 1
+table_inner_border_thickness = 1
+table_outer_border_color = "#000000"
+table_inner_border_color = "#CCCCCC"
+font_type = "Open Sans"
+highlight_color = "#0165FC"
