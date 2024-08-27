1a2e6767573f02a6b910246693ed6cc8ca266cc3
rt-desai <abhyuday.desai@readytensor.com>
2024-08-27 13:47:15 -0700

    add table svg gen to run_all

--- src/run_all.py
+++ src/run_all.py
@@ -1,7 +1,6 @@
-from config import paths
-from utils import read_csv_as_df
 from f1_calculate_metrics import calculate_metrics
 from f2_summarize_metrics import summarize_metrics
+from f3_create_table_svgs import generate_table_svgs
 from f4_create_charts import create_charts
 from f5_run_statistical_tests import run_statistical_tests
 
@@ -9,5 +8,6 @@ from f5_run_statistical_tests import run_statistical_tests
 if __name__ == "__main__":
     calculate_metrics()
     summarize_metrics()
+    generate_table_svgs()
     create_charts()
     run_statistical_tests()
