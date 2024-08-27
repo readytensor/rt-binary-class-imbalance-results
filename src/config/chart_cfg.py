7d45d5afa5e575eb847162f83ee9b7ae4a7a24e6
rt-desai <abhyuday.desai@readytensor.com>
2024-08-27 06:17:52 -0700

    clean up in chart cfg

--- src/config/chart_cfg.py
+++ src/config/chart_cfg.py
@@ -1,19 +1,16 @@
-# chart config
-chart_cfg = {
-    "border_visible": False,
-    "transparency": True,
-    "title_font_size": 20,
-    "xlabel_font_size": 16,
-    "ylabel_font_size": 16,
-    "tick_font_size": 12,
-    "font_color": "#000000",
-    "legend_font_color": "#000000",
-    "legend_font_size": 16,
-    "colors": {
-        "baseline": "#7986CB",
-        "class_weights": "#f5b507",
-        "decision_threshold": "#5BC8A0",
-        "smote": "#FF9B61",
-        "tie": "#B3BEC4",
-    },
+border_visible = False
+transparency = True
+title_font_size = 20
+xlabel_font_size = 16
+ylabel_font_size = 16
+tick_font_size = 12
+font_color = "#000000"
+legend_font_color = "#000000"
+legend_font_size = 16
+colors = {
+    "Baseline": "#7986CB",
+    "Class Weights": "#f5b507",
+    "Decision Threshold": "#5BC8A0",
+    "SMOTE": "#FF9B61",
+    "Tie": "#B3BEC4",
 }
