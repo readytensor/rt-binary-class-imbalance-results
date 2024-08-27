650cecb2335951b4fe94657277d95328ff113522
rt-desai <abhyuday.desai@readytensor.com>
2024-08-27 05:37:08 -0700

    reorg repo

---
+++ src/config/chart_cfg.py
@@ -0,0 +1,19 @@
+# chart config
+chart_cfg = {
+    "border_visible": False,
+    "transparency": True,
+    "title_font_size": 20,
+    "xlabel_font_size": 16,
+    "ylabel_font_size": 16,
+    "tick_font_size": 12,
+    "font_color": "#000000",
+    "legend_font_color": "#000000",
+    "legend_font_size": 16,
+    "colors": {
+        "baseline": "#7986CB",
+        "class_weights": "#f5b507",
+        "decision_threshold": "#5BC8A0",
+        "smote": "#FF9B61",
+        "tie": "#B3BEC4",
+    },
+}
